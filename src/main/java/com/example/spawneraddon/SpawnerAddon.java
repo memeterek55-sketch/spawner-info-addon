4fb8e4160f3b7e22443726f15fdbfffffb05e045
memeterek55-sketch <memeterek55@gmail.com>
2026-05-25 15:06:56 +0300

    fix: ozel kategori kaldirildi, Categories.Render kullaniliyor

--- src/main/java/com/example/spawneraddon/SpawnerAddon.java
+++ src/main/java/com/example/spawneraddon/SpawnerAddon.java
@@ -2,17 +2,9 @@ package com.example.spawneraddon;
 
 import com.example.spawneraddon.modules.SpawnerInfo;
 import meteordevelopment.meteorclient.addons.MeteorAddon;
-import meteordevelopment.meteorclient.systems.modules.Category;
 import meteordevelopment.meteorclient.systems.modules.Modules;
 
 public class SpawnerAddon extends MeteorAddon {
-
-    public static final Category KATEGORI = new Category("SpawnerInfo");
-
-    @Override
-    public void addCategories() {
-        Modules.get().addCategory(KATEGORI);
-    }
 
     @Override
     public void onInitialize() {
