06c4c61dfdefa91ec42592149b7dd06436a09ace
memeterek55-sketch <memeterek55@gmail.com>
2026-05-25 14:57:06 +0300

    feat: ozel SpawnerInfo kategorisi olusturuldu

--- src/main/java/com/example/spawneraddon/SpawnerAddon.java
+++ src/main/java/com/example/spawneraddon/SpawnerAddon.java
@@ -2,11 +2,12 @@ package com.example.spawneraddon;
 
 import com.example.spawneraddon.modules.SpawnerInfo;
 import meteordevelopment.meteorclient.addons.MeteorAddon;
+import meteordevelopment.meteorclient.systems.modules.Category;
 import meteordevelopment.meteorclient.systems.modules.Modules;
 
 public class SpawnerAddon extends MeteorAddon {
 
-    public static final String NAME = "Spawner Info";
+    public static final Category KATEGORI = new Category("SpawnerInfo");
 
     @Override
     public void onInitialize() {
