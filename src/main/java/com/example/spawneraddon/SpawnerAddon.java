1881ea6c2886160b9f1c79ca0d8fb5a25871c24f
memeterek55-sketch <memeterek55@gmail.com>
2026-05-25 14:09:55 +0300

    add src/main/java/com/example/spawneraddon/SpawnerAddon.java

---
+++ src/main/java/com/example/spawneraddon/SpawnerAddon.java
@@ -0,0 +1,20 @@
+package com.example.spawneraddon;
+
+import com.example.spawneraddon.modules.SpawnerInfo;
+import meteordevelopment.meteorclient.addons.MeteorAddon;
+import meteordevelopment.meteorclient.systems.modules.Modules;
+
+public class SpawnerAddon extends MeteorAddon {
+
+    public static final String NAME = "Spawner Info";
+
+    @Override
+    public void onInitialize() {
+        Modules.get().add(new SpawnerInfo());
+    }
+
+    @Override
+    public String getPackage() {
+        return "com.example.spawneraddon";
+    }
+}
